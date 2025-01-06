8e15fa70fd4d9428d46e190471e64da756b5bea9
Ahmed-Messaoudi <ahmed.massoudi@horizon-tech.tn>
2025-01-06 01:03:03 +0100

    button component

---
+++ components/Button.tsx
@@ -0,0 +1,31 @@
+// components/Button.js
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+export default function Button({ title, onPress }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
